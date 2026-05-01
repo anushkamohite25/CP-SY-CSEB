2cae523b588b6470a8cf013ef70983befd5ff44e
anushkamohite25 <anushka.mohite@mitwpu.edu.in>
2026-05-01 14:01:55 +0530

    Create Q1.cpp

---
+++ EXP 8/Q1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    string s;
+    cout << "Enter string: ";
+    cin >> s;
+    string rev = s;
+    reverse(rev.begin(), rev.end());
+    if(s == rev) cout << "Palindrome";
+    else cout << "Not Palindrome";
+}
