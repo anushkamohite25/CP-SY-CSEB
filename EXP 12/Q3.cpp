bbfbea60f9dc8fd3a66ec78d6adffdbd0e064efe
anushkamohite25 <anushka.mohite@mitwpu.edu.in>
2026-05-01 14:13:46 +0530

    Create Q3.cpp

---
+++ EXP 12/Q3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 4;
+    int adj[4][4] = {
+        {0,1,1,1},
+        {1,0,1,0},
+        {1,1,0,0},
+        {1,0,0,0}
+    };
+    int edges = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (adj[i][j] == 1)
+                edges++;
+        }
+    }
+    cout << "Total edges: " << edges/2;
+    return 0;
+}
