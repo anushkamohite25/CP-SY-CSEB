f9a109cd78268f4ffe27d02df66516b03582feaf
anushkamohite25 <anushka.mohite@mitwpu.edu.in>
2026-05-01 13:22:26 +0530

    Update Q2.cpp

--- EXP 6/Q2.cpp
+++ EXP 6/Q2.cpp
@@ -1,37 +1,27 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-
-int main()
-{
-    int n, key;
-
-    cout << "Enter size: ";
-    cin >> n;
-
-    int a[n];
-
-    cout << "Enter sorted array: ";
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-
-    cout << "Enter element to find first occurrence: ";
-    cin >> key;
-
-    int index = -1;
-
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i] == key)
-        {
-            index = i;
-            break;
-        }
+int binarySearch(const vector<int>& arr, int target, int left, int right) {
+    if (left > right) {
+        return -1; 
     }
-
-    if (index != -1)
-        cout << "First occurrence at index: " << index;
-    else
-        cout << "Element not found";
-
+    int mid = left + (right - left) / 2;
+    if (arr[mid] == target) {
+        return mid;
+    } else if (arr[mid] < target) {
+        return binarySearch(arr, target, mid + 1, right);
+    } else {
+        return binarySearch(arr, target, left, mid - 1);
+    }
+}
+int main() {
+    vector<int> arr = {1, 3, 5, 7, 9, 11, 13};
+    int target = 7;
+    int result = binarySearch(arr, target, 0, arr.size() - 1);
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    } else {
+        cout << "Element not found." << endl;
+    }
     return 0;
 }
